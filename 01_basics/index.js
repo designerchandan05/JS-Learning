52a99c884c0e7cd585ab50d8ada4fed44386fad3
Chandan <designerchandan05@gmail.com>
2024-12-26 04:51:20 +0000

    Comparison JS

---
+++ 01_basics/index.js
@@ -0,0 +1,8 @@
+console.log('Hey World');
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
