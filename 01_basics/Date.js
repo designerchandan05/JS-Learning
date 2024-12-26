1ed86226fb4612e8cd879b83ffbe3ef9d0332861
Chandan <designerchandan05@gmail.com>
2024-12-26 14:11:05 +0000

    Date and TIme

---
+++ 01_basics/Date.js
@@ -0,0 +1,8 @@
+const date = new Date();
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString())
+console.log(date.toLocaleTimeString())
+console.log(date.toUTCString())
+console.log(date.getTimezoneOffset())
