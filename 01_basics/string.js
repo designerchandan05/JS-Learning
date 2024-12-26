28a34b0ba0fc00592fff7d14bf08c23fd6c384cf
Chandan <designerchandan05@gmail.com>
2024-12-26 13:39:26 +0000

    String Exploring

---
+++ 01_basics/string.js
@@ -0,0 +1,15 @@
+const firstName = "Chandan";
+const ageYr = 32;
+
+console.log(`I am ${firstName} and my age is ${ageYr}`);
+
+const newStrng = new String ('My Name is Sachin');
+const newStrngSpace = new String ('  Hi This is Rahul Lakhera        ');
+
+console.log(newStrng.slice(0, 2));
+console.log(newStrng.charAt(3));
+console.log(newStrngSpace.trim());
+console.log(newStrngSpace.toUpperCase().trim())
+console.log(newStrngSpace.trim().split())
+console.log(newStrngSpace.trim().substring(2, 4))
+console.log(newStrngSpace.slice(1, 4))
